ab20e6e35329677f1923b5bd4abcb0d5064b7f75
kcs-pradhyumansinhchavda <60393187+kcs-pradhyumansinhchavda@users.noreply.github.com>
2024-09-25 06:04:05 +0000

    Add request dependency and add API request call to fruityvice third party

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customie Your Smoothies :cup_with_straw:")
@@ -38,5 +39,7 @@ if ingredient_list:
         st.success("""Your smoothie is ordered, '"""+ name_on_order+"""'""")
         st.stop()
 
-
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     
